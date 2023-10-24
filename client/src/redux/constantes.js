a32fa552e128b793eb7acecb3da212571cc0fb52
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-24 17:09:35 -0300

    recupero carpeta redux de api

---
+++ client/src/redux/constantes.js
@@ -0,0 +1,16 @@
+export const GET_ALL_VG = "GET_ALL_VG";
+export const GET_DB_VGS = "GET_DB_VGS";
+export const GET_API_VGS = "GET_API_VGS";
+export const GET_VG_BY_ID = "GET_VG_BY_ID";
+export const GET_VG_BY_NAME = "GET_VG_BY_NAME";
+export const GET_GENRES = "GET_GENRES";
+export const ORDER_BY = "ORDER_BY";
+export const FILTER_BY = "FILTER_BY";
+export const RATING_BY = "RATING_BY";
+export const GET_PLAT = "GET_PLAT";
+
+// export const CLEAR_VG_BY_FORM = "CLEAR_VG_BY_FORM";
+
+// export const SORT_VG = "SORT_VG";
+
+
