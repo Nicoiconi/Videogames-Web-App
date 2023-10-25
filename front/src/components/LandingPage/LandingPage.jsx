ae08f9d4eadde3b4343e619cd6b8c8b76460e31d
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-25 13:32:17 -0300

    cambia LandingPage

--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -2,14 +2,17 @@ import "./LandingPage.css"
 import LinksBar from "../LinksBar/LinksBar";
 
 export default function LandingPage() {
+
+  const RAWG_API = "https://rawg.io/apidocs"
+
   return (
     <div className="landing-page">
+
+      <LinksBar />
+
       <h1>Bienvenidos</h1>
 
-      <h6>
-      <LinksBar />
-      </h6>
-
+      <p>Aplicación web con temática de videojuegos utilizando <a href={RAWG_API} target="_blank" rel="noreferrer">RAWG API</a></p>
 
     </div>
   )
