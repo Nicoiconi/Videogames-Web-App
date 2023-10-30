1338aee86a2357aecb2ba5f45a99f8f106524a6d
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-29 21:27:33 -0300

    video game detail style

--- front/src/components/VideoGameDetail/VideoGameDetail.jsx
+++ front/src/components/VideoGameDetail/VideoGameDetail.jsx
@@ -4,6 +4,7 @@ import HomeLink from "../HomeLink/HomeLink"
 import LinksBar from "../LinksBar/LinksBar"
 import "./VideoGameDetail.css"
 import { getSingleVideoGame } from "../../utils/api"
+import StarRating from "../RatingStars/RatingStars"
 
 export default function VideoGameDetail() {
 
@@ -11,7 +12,7 @@ export default function VideoGameDetail() {
 
   const [videoGameToRender, setVideoGameToRender] = useState("")
 
-  const storedVideoGame = window.localStorage.getItem('stored-video-game') 
+  const storedVideoGame = window.localStorage.getItem('stored-video-game')
 
   useEffect(() => {
     if (storedVideoGame) {
@@ -25,15 +26,77 @@ export default function VideoGameDetail() {
     }
   }, [singleVideoGame])
 
+  console.log(videoGameToRender)
+
   return (
     <div className="videogame">
 
       <LinksBar />
       <HomeLink />
 
-      <div className="container-fluid">
-        {videoGameToRender?.name}
-        <img src={videoGameToRender?.background_image} alt="" />
+      <div className="container-fluid single-game">
+
+        <div className="row image-data">
+          <div className="col">
+            <img
+              className="image"
+              src={videoGameToRender?.background_image}
+              alt=""
+            />
+          </div>
+          <div className="col">
+            {videoGameToRender?.name}
+            <hr />
+            <StarRating score={videoGameToRender?.rating} />
+
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row platforms-game-detail-container">
+          <div className="col">
+            Platforms:
+          </div>
+          <div className="col platforms-game-detail-list">
+            {
+              videoGameToRender?.platforms?.map(p => {
+                return (
+                  <div key={`platform-${p?.platgform?.id}`}>
+                    {p?.platform?.name}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+
+        <div className="row genres-game-detail-container">
+          <div className="col">
+            Genres:
+          </div>
+          <div className="col genres-game-detail-list">
+            {
+              videoGameToRender?.genres?.map(g => {
+                return (
+                  <div key={`genre-${g?.id}`}>
+                    {g?.name}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row game-detail-description">
+
+          <h4 className="game-detail-description-title">
+            Description
+          </h4>
+          {videoGameToRender?.description_raw}
+        </div>
       </div>
     </div>
   )
