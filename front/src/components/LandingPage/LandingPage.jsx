0d6c9d1201d062a9d122aa2e2ed845e2bb568709
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-29 21:37:17 -0300

    - App + landing page description

--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -14,6 +14,10 @@ export default function LandingPage() {
 
       <p>Aplicación web con temática de videojuegos utilizando <a href={RAWG_API} target="_blank" rel="noreferrer">RAWG API</a></p>
 
+      <p>A tener en cuenta: el boton `Store Video Games` guarda los juegos en el navegador. No olvide presionar `Clear Stored Video Games` antes de salir.</p>
+
+      <p>Gracias por pasar! Espero te guste y te sirva.</p>
+
     </div>
   )
 }
